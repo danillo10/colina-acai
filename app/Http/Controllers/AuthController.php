0a81f1be60336570c986ac991f086ad6a760ed0f
Danillo Leão Deucher <danillodeucher@movida.com.br>
2025-04-02 20:40:38 -0300

    correção da funcao

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -112,6 +112,6 @@ class AuthController extends Controller
      */
     public function me()
     {
-        return response()->json(auth('api')->user());
+        return response()->json(auth()->user());
     }
 }
