868418bbeb38e287ccf5b46d1cfb3a990eddefbb
Danillo Leão Deucher <danillodeucher@movida.com.br>
2025-04-02 18:54:13 -0300

    adição ao fillable de users

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'nome_pessoa',
+        'nome_empresa',
+        'cpf_cnpj',
+        'telefone'
     ];
 
     /**
